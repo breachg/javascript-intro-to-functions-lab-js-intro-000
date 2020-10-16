ced6493bba6c054b39f2cdcfb60cbc0086217ab9
Garrett Breach <breachg1992@gmail.com>
2020-10-16 19:49:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ function whisper(string) {
   return string.toLowerCase ()
 }
 
-function shout(string) {
-  console.log "string.toUpperCase"()
-}
-
-function whisper(string) {
-  console.log string.toLowerCase ()
+function logShout(string) {
+  console.log string.toUpperCase ()
 }
